bf15465880e289a1dd0123f6868cefd0ffb84a4c
Alena <shining8smaragd@gmail.com>
2026-03-03 14:57:26 +0200

    responsive pictures

--- images/burger-button-icon.jsx
+++
@@ -1,1 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20"><path fill="currentColor" fillRule="evenodd" d="M3 5a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1" clipRule="evenodd"></path></svg>
